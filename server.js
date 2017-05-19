22c7eddbbe3834fa7d80cd459346b190e2c6a741
Sean McElhiney <sean.mcelhiney@me.com>
2017-05-19 15:48:15 +0000

    Make request to external website

--- server.js
+++ server.js
@@ -8,7 +8,25 @@ var PORT = 8081;
 var app = express();
 
 app.get('/scrape', function(req, res){
+  //URL to scrape - Anchorman 2
+  url = 'http://www.imdb.com/title/tt1229340/';
 
+  //Request call
+  //First parameter is URL
+  //Callback takes 3 parameters, an error, responce status code, and the html
+  request(url, function(error, responce, html){
+
+    //First, check to ensure no errors are received from responce
+    if(!error) {
+
+      //Next, use cheerio on returned html for jquery-like functionaility
+      var $ = cheerio.load(html);
+
+      //Finally, define varibles to capture
+      var title, release, rating;
+      var json = { title : "", release : "", rating : ""};
+    }
+  })
 })
 
 
